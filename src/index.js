0e1c36c8301de56a24a638c1ba458ff72f2005da
ShunshunL <shunshunlyu@protonmail.com>
2020-04-21 14:29:43 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // let submitButton = document.getElementById('submit-button');
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', submit);
 });
+
+function submit(e) {
+  e.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  let listItem = document.createElement('li');
+  const tasks = document.getElementById('tasks')
+  listItem.innerText = newTaskDescription.value;
+  tasks.appendChild(listItem);
+  event.target.reset();
+}
+
